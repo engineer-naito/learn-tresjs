58bd50d3612a0d9bdcfd9493e468b8591f235274
engineer-naito <kosuke.naito.engineer@gmail.com>
2025-02-02 20:17:04 +0900

    Update TresMesh properties and add TresAxesHelper to TresCanvas component

--- app.vue
+++ app.vue
@@ -1,8 +1,13 @@
 <template>
   <TresCanvas window-size clear-color="#82DBC5">
-    <TresMesh>
-      <TresBoxGeometry :args="[1,2,3]"/>
+    <TresMesh 
+      :position="[0, 1, 0]" 
+      :scale="[2, 0.5, 1]" 
+      :rotation="[Math.PI / 4, -Math.PI / 3, 0]"
+    >
+      <TresBoxGeometry :args="[1, 1, 1]" />
       <TresMeshNormalMaterial />
     </TresMesh>
+    <TresAxesHelper :args="[5]" />
   </TresCanvas>
 </template>
